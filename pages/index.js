24e4688b368052769e5cab97910b96388c35b208
Reginaldo Bonaldi <reginaldobonaldi22@gmail.com>
2021-01-30 17:00:39 -0300

    eslint, formulário  de nome para o quiz, useState e router

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,13 @@
-import db from '../db.json'
-import styled from 'styled-components'
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import QuizLogo from '../src/components/QuizLogo'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import React from 'react';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
-
-
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -24,34 +20,48 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-      <QuizLogo></QuizLogo>
-      <Widget> 
-        <Widget.Header>
-
-        </Widget.Header>
-        <Widget.Content>
-          <h1>Meu Quiz</h1>
-        </Widget.Content>
-      </Widget>
-      <Widget> 
-        <Widget.Header>
-          
-        </Widget.Header>
-        <Widget.Content>
-        <h1>Quizes da galera</h1>
-
-        </Widget.Content>
-      </Widget>
-      <Footer></Footer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Nome do Quiz</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (event) {
+              event.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <input
+                onChange={function (event) {
+                  setName(event.target.value);
+                }}
+                placeholder="Como posso lhe chamar?"
+              />
+              <button type="submit" disabled={!name}>
+                Vamos jogar
+                {name}
+                ?
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da galera</h1>
+          </Widget.Header>
+          <Widget.Content />
+        </Widget>
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/regisbaldo"/>
+      <GitHubCorner projectUrl="https://github.com/regisbaldo" />
     </QuizBackground>
-
 
   );
 }

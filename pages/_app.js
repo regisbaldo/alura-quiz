4a67596f50936ec1d19b1ebbb8e6845a0bf8c665
Reginaldo Bonaldi <reginaldobonaldi22@gmail.com>
2021-01-30 17:35:25 -0300

    adicionando informações no db e metas corretas

--- pages/_app.js
+++ pages/_app.js
@@ -33,21 +33,22 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Space Quiz - Alura</title>
-        <meta name="title" content="Space Quiz - Alura" />
-        <meta name="description" content="Quiz  criado a partir da imersão react da Alura" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+        <meta name="title" content={db.title} />
+        <meta name="description" content={db.description} />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
-        <meta property="og:title" content="Space Quiz - Alura" />
-        <meta property="og:description" content="Quiz  criado a partir da imersão react da Alura " />
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="og:url" content={db.url} />
+        <meta property="og:title" content={db.title} />
+        <meta property="og:description" content={db.description} />
+        <meta property="og:image" content={db.bg} />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
-        <meta property="twitter:title" content="Space Quiz - Alura" />
-        <meta property="twitter:description" content="Quiz  criado a partir da imersão react da Alura " />
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="twitter:url" content={db.url} />
+        <meta property="twitter:title" content={db.title} />
+        <meta property="twitter:description" content={db.description} />
+        <meta property="twitter:image" content={db.bg} />
 
       </Head>
       <ThemeProvider theme={theme}>
